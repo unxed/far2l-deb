705c30d368c363fec4ac717664ca6a016c416605
unxed <unxed@mail.ru>
2021-03-01 00:44:15 +0300

    Update autonomizer.sh

--- autonomizer.sh
+++ autonomizer.sh
@@ -16,17 +16,17 @@ for file in ./**/* ; do
         str="${str}/lib"
         #echo $str
         echo $file
-        ./patchelf --remove-rpath $file
+        patchelf --remove-rpath $file
         #ldd $file |awk '{if(substr($3,0,1)=="/") print $1,$3}' | cut -d\  -f2 | \
         ldd $file | grep "=> /" | awk '{print $1, $3}' | cut -d\  -f2 | \
         xargs -d '\n' -I{} cp --copy-contents {} ./lib
-        ./patchelf --set-rpath $str $file
+        patchelf --set-rpath $str $file
     fi
 done
 for file in ./lib/* ; do
     if file $file | grep ELF > /dev/null; then
         echo $file
-        ./patchelf --remove-rpath $file
-        ./patchelf --set-rpath "\$ORIGIN" $file
+        patchelf --remove-rpath $file
+        patchelf --set-rpath "\$ORIGIN" $file
     fi
 done
