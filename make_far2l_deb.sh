223b4222af77bdfdcecb2fa184bc850353382c0b
unxed <unxed@mail.ru>
2021-03-01 05:23:19 +0300

    Update make_far2l_deb.sh

--- make_far2l_deb.sh
+++ make_far2l_deb.sh
@@ -36,7 +36,7 @@ mkdir deb
 mkdir deb/far2l
 mkdir deb/far2l/DEBIAN
 echo "Package: far2l" > deb/far2l/DEBIAN/control
-echo "Version: 2.2" >> deb/far2l/DEBIAN/control
+echo "Version: 2.3" >> deb/far2l/DEBIAN/control
 MACHINE_TYPE=`uname -m`
 if [ ${MACHINE_TYPE} == 'x86_64' ]; then
     DEB_ARCH="amd64"
